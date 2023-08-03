2e560a4ea71881dbf936bc4175908c54393a760b
Gana Sai Teja <velichetiganasaiteja@gmail.com>
2023-08-03 22:25:40 +0530

    added builder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import { useEffect,useState } from "react";
-import {Routes,Route, Navigate} from "react-router-dom";
-import { useSelector, useDispatch } from 'react-redux'
-import { login} from './features/auth/authSlice'
-import {fetchPosts,fetchFollowingPosts} from './features/post/postSlice';
+import React, { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from './features/auth/authSlice';
+import { fetchPosts, fetchFollowingPosts } from './features/post/postSlice';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Header from "./components/Header";
@@ -11,71 +11,89 @@ import Profile from "./pages/Profile";
 import Post from "./pages/Post";
 import Bookmarks from "./pages/Bookmarks";
 import Following from "./pages/Following";
+
 function App() {
+  // Initialize Redux dispatch function
   const dispatch = useDispatch();
-  const [user,setUser] = useState(null);
+
+  // Local state to manage the user and posts data
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState(null);
+  const [followingPosts, setFollowingPosts] = useState(null);
+
+  // Get authentication and post state from Redux store
   const auth = useSelector((state) => state.auth);
-  const [posts,setPosts] = useState(null);
   const post = useSelector((state) => state.post);
-  const [followingPosts,setFollowingPosts] = useState(null);
+
+  // Dispatch the login action once when the component mounts
   useEffect(() => {
     if (auth.status === 'idle') {
       dispatch(login());
     }
-    if(auth.status === 'succeeded'){
-      setUser(auth.user);
-    }
-    if(auth.status === 'failed'){
-      setUser(null);
-    }
   }, [auth.status, dispatch]);
 
+  // Update the local user state based on the authentication status
+  useEffect(() => {
+    if (auth.status === 'succeeded') {
+      setUser(auth.user);
+    } else if (auth.status === 'failed') {
+      setUser(null);
+    }
+  }, [auth.status]);
 
+  // Fetch the posts once when the component mounts
   useEffect(() => {
     if (post.status === 'idle') {
       dispatch(fetchPosts());
     }
-    if(post.status === 'succeeded'){
+  }, [post.status, dispatch]);
+
+  // Update the local posts state based on the fetch status
+  useEffect(() => {
+    if (post.status === 'succeeded') {
       setPosts(post.posts);
-    }
-    if(post.status === 'failed'){
+    } else if (post.status === 'failed') {
       setPosts(null);
     }
-  }, [post.status, dispatch]);
+  }, [post.status]);
 
-
-  
+  // Fetch the following posts once when the component mounts and user data is available
   useEffect(() => {
-    if (post.status === 'idle') {
-      if(user)
+    if (post.status === 'idle' && user) {
       dispatch(fetchFollowingPosts(user._id));
     }
-    if(post.status === 'succeeded'){
+  }, [post.status, dispatch, user]);
+
+  // Update the local followingPosts state based on the fetch status
+  useEffect(() => {
+    if (post.status === 'succeeded') {
       setFollowingPosts(post.followingPosts);
-    }
-    if(post.status === 'failed'){
+    } else if (post.status === 'failed') {
       setFollowingPosts(null);
     }
-  }, [post.status, dispatch]);
+  }, [post.status]);
+
+  // Define the paths for routing
   const write = user ? `/post/create/${user._id}` : '/';
-  const following=user? `/home/following/${user._id}` : `/`;
+  const following = user ? `/home/following/${user._id}` : `/`;
+
+  // Return the JSX for the App component
   return (
     <>
-        <div>
-        <Header user={user}/>
-          <Routes>
-            <Route path="/" element={user? <Navigate to ="/home"/>:<Login/>}/>
-            <Route path="/home" element={<Home/>}/>
-            <Route path={following} element={<Home/>}/>
-            <Route path= {write} element={<BlogInput/>}/>
-            <Route path="/profile/:id" element={<Profile/>}/>
-            <Route path="/post/:id" element={<Post/>}/>
-            <Route path="/bookmarks" element={<Bookmarks/>}/>
-            <Route path="/user/following" element={<Following/>}/>
-          </Routes>
-        </div>
+      <div>
+        <Header user={user} />
+        <Routes>
+          <Route path="/" element={user ? <Navigate to="/home" /> : <Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path={following} element={<Home />} />
+          <Route path={write} element={<BlogInput />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/bookmarks" element={<Bookmarks />} />
+          <Route path="/user/following" element={<Following />} />
+        </Routes>
+      </div>
     </>
-    
   );
 }
 
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -30,30 +30,6 @@ export const login= createAsyncThunk('auth/login',async()=>{
 
 })
 
-export const logout= createAsyncThunk('auth/logout',async()=>{
-    const response = await fetch(`${process.env.REACT_APP_API}/auth/logout`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("authentication has been failed!");
-        })
-        .then((resObject) => {
-          return resObject.user;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-        return response;
-
-})
-
 export const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -62,36 +38,20 @@ export const authSlice = createSlice({
             state.user = action.payload;
         }
     },
-    extraReducers:{
-        [login.pending]:(state,action)=>{
-            state.status="loading";
-        }
-        ,
-
-        [login.fulfilled]:(state,action)=>{
-            state.status="succeeded";
-            state.user=action.payload;
-        }
-        ,
-        [login.rejected]:(state,action)=>{
-            state.status="failed";
-            state.error=action.error.message;
-        }
-        ,
-        [logout.pending]:(state,action)=>{
-            state.status="loading";
-        } 
-        ,
-        [logout.fulfilled]:(state,action)=>{
-            state.status="succeeded";
-            state.user=null;
-        }
-        ,
-        [logout.rejected]:(state,action)=>{
-            state.status="failed";
-            state.error=action.error.message;
-        }
-    }
+    extraReducers: (builder) => { // Use builder to define extraReducers
+      builder
+          .addCase(login.pending, (state) => {
+              state.status = "loading";
+          })
+          .addCase(login.fulfilled, (state, action) => {
+              state.status = "succeeded";
+              state.user = action.payload;
+          })
+          .addCase(login.rejected, (state, action) => {
+              state.status = "failed";
+              state.error = action.error.message;
+          })
+  },
 });
 export const {setUser} = authSlice.actions;
 export default authSlice.reducer;
